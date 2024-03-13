ccbd0fc919c45aa5f9101ae9bb2d1d3f1f3e24dd
Aneek Roy <aneeknroy@gmail.com>
2024-03-13 17:33:33 -0500

    Got my stupid crud app working, yayyy

--- groceryList/app.js
+++ groceryList/app.js
@@ -1,15 +1,37 @@
-// Set Initial Count
-let numOfTasks = 0;
+// // Set Initial Count
+// let numOfTasks = 0;
 
-// Set Grocery List
-const groceryList = []
+// // Set Grocery List
+// const groceryList = []
 
-// Select Value and Buttons
-const task = document.querySelector('#task');
-const btn = document.querySelectorAll('.btn')
+// // Select Value and Buttons
+// const task = document.querySelector('#task');
+// const btn = document.querySelectorAll('.btn')
 
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('groceryForm');
     const itemList = document.getElementById('itemList')
-})
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const itemInput = document.getElementById('item');
+        const itemValue = itemInput.value.trim();
+        if (itemValue !== ''){
+            addItem(itemValue);
+            itemInput.value = '';
+        }
+    });
+
+    function addItem(item) {
+        const li = document.createElement('li');
+        li.textContent = item;
+        const deleteButton = document.createElement('button');
+        deleteButton.addEventListener('click', function() {
+            itemList.removeChild(li);
+        });
+        li.appendChild(deleteButton);
+        itemList.appendChild(li);
+    }   
+    
+});
