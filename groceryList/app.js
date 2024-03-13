0a8ffb6d44be545e3d69ddcbb4c724a5db2de442
Aneek Roy <aneeknroy@gmail.com>
2024-03-13 17:26:14 -0500

    did something

---
+++ groceryList/app.js
@@ -0,0 +1,15 @@
+// Set Initial Count
+let numOfTasks = 0;
+
+// Set Grocery List
+const groceryList = []
+
+// Select Value and Buttons
+const task = document.querySelector('#task');
+const btn = document.querySelectorAll('.btn')
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('groceryForm');
+    const itemList = document.getElementById('itemList')
+})
